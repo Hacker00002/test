aebd668d48d9745e099ec97189c06ea0e3bf3bb4
Hacker00002 <abdulbositsharipovv@gmail.com>
2023-01-14 21:54:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let arr = [
+  "color1",
+  "color2",
+  "color3",
+  "color4",
+  "color5",
+  "color6",
+  "color7",
+  "color8",
+  "color9",
+];
+
+console.log(arr[2]);
+
+console.log((arr[5] = "hhh"));
+
+console.log(arr);
+
+const color = "The all colors " + arr;
+
+// console.log(color);
+
+// let arrTwo = [];
+
+// arrTwo[0] = "color1";
+// arrTwo[1] = "color1";
+// arrTwo[2] = "color1";
+// arrTwo[3] = "color1";
+
+// console.log(arrTwo);
+
+// let arrThree = new Array(true, new Date(), 526, "string");
+
+// console.log(arrThree);
